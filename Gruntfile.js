c608ac5e9636e92e5d32516091af8c631ee4aacd
Pilwon Huh <pilwon@gmail.com>
2013-11-13 07:59:34 -0500

    Updated Gruntfile and package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -19,32 +19,18 @@ module.exports = function (grunt) {
         '!node_modules/**/*'
       ]
     },
-    mochaTest: {  // grunt-mocha-test
-      files: ['test/**/*.js']
-    },
-    mochaTestConfig: {  // grunt-mocha-test
-      options: {
-        reporter: 'nyan'
-      }
-    },
-    watch: {  // grunt-regarde (task renamed from regarde to watch)
+    watch: {  // grunt-contrib-watch
       all: {
-        files: '**/*.js'
+        files: [
+          '**/*.js',
+          '!node_modules/**/*'
+        ]
       }
     }
   });
 
-  grunt.renameTask('regarde', 'watch');
-
-  grunt.registerTask('test', [
-    'jshint',
-    'mochaTest',
-    'watch'
-  ]);
-
-  grunt.registerTask('t', 'test');
-
   grunt.registerTask('default', [
-    'test'
+    'jshint',
+    'watch'
   ]);
 };
